1f371c7b7aa2df0fc9975226a2c97805fec5f21e
daniilkuz <d.d.kuzovlev@gmail.com>
2024-05-14 02:18:09 +0700

    Added sturct's

--- main.go
+++ main.go
@@ -1,12 +1,47 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
+
+type Block struct {
+	Position  int
+	Data      BookCheckout
+	TimeStamp string
+	Hash      string
+	PrevHash  string
+}
+
+type BookCheckout struct {
+	BookID       string `json:"book_id"`
+	User         string `json:"user"`
+	CheckoutDate string `json:"checkout_date"`
+	IsGenesis    bool   `json:"is_genesis"`
+}
+
+type Book struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	PublishDate string `json:"publish_date"`
+	ISBN        string `json:"isbn"`
+}
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+var Blockxhain *Blockchain
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", getBlockchain).Methods("GET")
 	r.HandleFunc("/", writeBlock).Methods("POST")
 	r.HandleFunc("/new", newBook).Methods("POST")
+
+	log.Println("Listening on port 3000")
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
